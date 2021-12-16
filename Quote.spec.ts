e85607f8df0d9a6d6998ba649a8a3a561c0d3e67
heyset <c.inaciomatheus@gmail.com>
2021-12-16 03:00:00 -0300

    Adds failing test for static method

--- Quote.spec.ts
+++ Quote.spec.ts
@@ -1,10 +1,24 @@
 import { describe, it, expect } from '@jest/globals';
 import { Quote } from './Quote';
 
-describe('Form constructor', () => {
-  it('constructs a new Form instance with default values', () => {
+describe('Quote constructor', () => {
+  it('constructs a new Quote instance with default values', () => {
     const testQuote = new Quote();
     expect(testQuote.author).toBe('Uncle Ben');
     expect(testQuote.content).toBe('With great power comes great responsibility.');
+  });
+});
+
+describe('Quote.merge', () => {
+  it('successfully creates a merged Quote', () => {
+    const uncleBenQuote = new Quote();
+    const linusQuote = new Quote({
+      author: 'Linus Torvald',
+      content: 'Talk is cheap. Show me the code.',
+    });
+
+    const mergedQuote = Quote.merge(uncleBenQuote, linusQuote);
+    expect(mergedQuote.author).toBe('Uncle Ben ft. Linus Torvald');
+    expect(mergedQuote.content).toBe('With great power comes great responsibility. Also, talk is cheap. Show me the code.');
   });
 });
