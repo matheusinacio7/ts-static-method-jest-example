7c67532bc8c2a7384abaf79cb81ef74498439791
heyset <c.inaciomatheus@gmail.com>
2021-12-16 03:06:04 -0300

    Adds Quote.merge

--- Quote.ts
+++ Quote.ts
@@ -4,8 +4,8 @@ export interface QuoteConstructor {
 }
 
 export class Quote {
-  author: string;
-  content: string;
+  public readonly author: string;
+  public readonly content: string;
 
   constructor({
     author = 'Uncle Ben',
@@ -13,5 +13,16 @@ export class Quote {
   }: QuoteConstructor = {}) {
     this.author = author;
     this.content = content;
+  }
+
+  public static merge(firstQuote: Quote, secondQuote: Quote) {
+    const author = `${firstQuote.author} ft. ${secondQuote.author}`;
+    const content = `${firstQuote.content} Also, ${Quote.uncapitalize(secondQuote.content)}`;
+    const mergedQuote = new Quote({ author, content });
+    return mergedQuote;
+  }
+
+  private static uncapitalize(sentence: string) {
+    return sentence[0].toLowerCase() + sentence.slice(1);
   }
 }
